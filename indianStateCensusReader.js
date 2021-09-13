49a70e79f05dbe374de886567e682e5be6c42a3d
unknown <sanjeevsingh.77d@gmail.com>
2021-09-14 00:45:30 +0530

    fectored code

--- indianStateCensusReader.js
+++ indianStateCensusReader.js
@@ -1,7 +1,11 @@
 const readFile = require('./indianStateCensus');
 let path = 'C:/Users/naps/Desktop/snjv/reactapp/IndianStateCensus/IndiaStateCensusData.csv';
 readFile(path).then((response) => {
-    console.log(response);
+    for(let i = 0; i < response.length - 1 ; i++) {
+        if(response[i].State === "Uttar Pradesh") {
+             console.log("the population of Uttar Pradesh is = " + response[i].Population)
+        }
+    }
 }).catch((error) => {
 
 })
