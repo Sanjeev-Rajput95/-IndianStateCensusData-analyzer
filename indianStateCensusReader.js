8383a19f6788dc59dc7896d055c36e01d19f4847
unknown <sanjeevsingh.77d@gmail.com>
2021-09-14 01:02:39 +0530

    the state name whose population = 10380463

--- indianStateCensusReader.js
+++ indianStateCensusReader.js
@@ -5,6 +5,9 @@ readFile(path).then((response) => {
         if(response[i].State === "Uttar Pradesh") {
              console.log("the population of Uttar Pradesh is = " + response[i].Population)
         }
+        if(response[i].Population == '103804637') {
+            console.log(" the state name whose population = 103804637 name is = " + response[i].State)
+        }
     }
 }).catch((error) => {
 
