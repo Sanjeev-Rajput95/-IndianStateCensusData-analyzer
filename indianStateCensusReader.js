60f9c2428c71ffd65c6541b2803a53688d24f5ac
unknown <sanjeevsingh.77d@gmail.com>
2021-09-15 18:22:36 +0530

    which is most populated DensityPerSqKm

--- indianStateCensusReader.js
+++ indianStateCensusReader.js
@@ -11,9 +11,9 @@ readFile(path).then((response) => {
     }
     console.log(parseInt(response[0].Population));
     for (let j = 0; j < response.length; j++) {
-      
+
         for (let k = j + 1; k < response.length; k++) {
-           
+
             if (parseInt(response[j].Population) > parseInt(response[k].Population)) {
                 let temp;
                 temp = response[j];
@@ -21,10 +21,29 @@ readFile(path).then((response) => {
                 response[k] = temp;
             }
         }
-     }
-     console.log(" the state name is = " + response[0].State + " \n state's population is least = " + response[0].Population )
-     console.log(" the state name is = " + response[response.length-1].State + " \n state's population is highest = " + response[response.length-1].Population )
-    
+    }
+    console.log(" the state name is = " + response[0].State + " \n state's population is least = " + response[0].Population)
+    console.log(" the state name is = " + response[response.length - 1].State + " \n state's population is highest = " + response[response.length - 1].Population)
+    for (let i = 0; i < response.length - 1; i++) {
+        if (response[i].State === "Uttar Pradesh") {
+            console.log("the population of Uttar Pradesh is = " + response[i].Population)
+        }
+        if (response[i].Population == '103804637') {
+            console.log(" the state name whose population = 103804637 name is = " + response[i].State)
+        }
+    }
+    for (let j = 0; j < response.length; j++) {
+        for (let k = j + 1; k < response.length; k++) {
+            if (parseInt(response[j].DensityPerSqKm) > parseInt(response[k].DensityPerSqKm)) {
+                let temp;
+                temp = response[j];
+                response[j] = response[k];
+                response[k] = temp;
+            }
+        }
+    }
+    console.log(" The Density of most populated is " + response[response.length - 1].DensityPerSqKm);
+
 }).catch((error) => {
 
 })
